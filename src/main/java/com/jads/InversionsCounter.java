3442d20069bb68a5e04e7e7a264a9594dc73c9a0
adio <fireball183@gmail.com>
2014-05-09 01:01:48 +0300

    remove inversion

--- src/main/java/com/jads/InversionsCounter.java
+++ src/main/java/com/jads/InversionsCounter.java
@@ -51,39 +51,5 @@ public class InversionsCounter {
 		return inversion;
 	}
 
-	private <T extends Comparable<? super T>> T[] merge(T[] left, T[] right) {
-		Comparable[] objects = new Comparable[left.length + right.length];
-
-		int i = 0;
-		int j = 0;
-		int k = 0;
-
-		while (i <= left.length - 1 && j <= right.length - 1) {
-
-
-			if (left[i].compareTo(right[j]) <= 0) {
-				objects[k] = left[i];
-				i++;
-			} else {
-				objects[k] = right[j];
-				j++;
-			}
-			k++;
-		}
-
-
-		while (i <= left.length - 1) {
-			objects[k] = left[i];
-			k++;
-			i++;
-		}
-
-		while (j <= right.length - 1) {
-			objects[k] = right[j];
-			k++;
-			j++;
-		}
-		return (T[]) objects;
-	}
 
 }
